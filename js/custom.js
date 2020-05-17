075184259a3005ea3648a555e2257139490282b0
asepsissolutions <65504004+asepsissolutions@users.noreply.github.com>
2020-05-17 22:59:50 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,12 @@
 // Add here all your JS customizations
+
+<script>
+      function disableClick(){
+        document.onclick=function(event){
+          if (event.button == 2) {
+            alert('Right Click Message');
+            return false;
+          }
+        }
+      }
+    </script>
